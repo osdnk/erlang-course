4d5b374978275e6d32e9c0233b08a81db21a6b14
osdnk <micosa97@gmail.com>
2018-04-13 20:50:39 +0200

    Add sample test :tada:

--- pollution.erl
+++ pollution.erl
@@ -10,8 +10,8 @@
 -author("osdnk").
 -export([create_monitor/0, add_station/3, get_all/1]).
 
--record(measurements, {all = sets:new(), type_date_station_to_meas = dict:new(), type_date_to_meas = dict:new(), type_station_to_meas = dict:new()}).
--record(stations, {all = sets:new(), coord_to_elem = dict:new(), name_to_elem = dict:new()}).
+-record(measurements, {all = sets:new(), type_date_station_to_meas = #{}, type_date_to_meas = #{}, type_station_to_meas = #{}}).
+-record(stations, {all = sets:new(), coord_to_elem = #{}, name_to_elem = #{}}).
 -record(monitor, {meas = #measurements{}, stats = #stations{}}).
 
 
@@ -25,23 +25,30 @@ create_monitor () ->
 add_station(Name, {X, Y}, Monitor) ->
   S = {Name, {X, Y}},
   NewAllStations = sets:add_element(S, (Monitor#monitor.stats)#stations.all),
-  Monitor#monitor{ stats = (Monitor#monitor.stats)#stations{ all = NewAllStations} }.
+  NewCoordsToElem = maps:put({X, Y}, S, (Monitor#monitor.stats)#stations.coord_to_elem),
+  NewNamesToElem = maps:put(Name, S, (Monitor#monitor.stats)#stations.name_to_elem),
+  Monitor#monitor{ stats = (Monitor#monitor.stats)#stations{
+    all = NewAllStations,
+    coord_to_elem = NewCoordsToElem,
+    name_to_elem = NewNamesToElem
+  } }.
+
+
+
 
 get_all (M) ->
   (M#monitor.stats)#stations.all.
 
 add_value({X, Y}, Date, Type, Value, Monitor) -> Monitor. %TODO
-%add_value(Name, Date, Type, Value, Monitor) -> (). %TODO
-%inner_add_value(Station, Date, Type, Value, Monitor) -> (). %TODO
-%remove_value({X, Y}, Date, Type, Monitor) -> (). %TODO
-%remove_value(Name, Date, Type, Monitor) -> (). %TODO
-%inner_remove_value(Station, Date, Type, Monitor) -> (). %TODO
+inner_add_value(Station, Date, Type, Value, Monitor) -> Monitor. %TODO
+remove_value({X, Y}, Date, Type, Monitor) -> Monitor. %TODO
+inner_remove_value(Station, Date, Type, Monitor) -> Monitor. %TODO
 
-%inner_get_one_value(Type, Date, Station) -> (). %TODO
+inner_get_one_value(Type, Date, Station, Monitor) -> Monitor. %TODO
 %get_one_value(Type, Date, L) -> inner_get_one_value(Type, Date, parse_date_or_coords_to_station()).
 
-%get_station_mean(Type, Date, Monitor) -> (). %TODO
-%get_daily_meann(Type, Date, Monitor) -> (). %TODO
+get_station_mean(Type, Date, Monitor) -> Monitor. %TODO
+get_daily_meann(Type, Date, Monitor) -> Monitor. %TODO
 
 
 
---
+++ pollution_tests.erl
@@ -0,0 +1,51 @@
+%%%-------------------------------------------------------------------
+%%% @author osdnk
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. Apr 2018 20:10
+%%%-------------------------------------------------------------------
+-module(pollution_tests).
+-author("osdnk").
+-include_lib("eunit/include/eunit.hrl").
+
+-record(measurements, {all = sets:new(), type_date_station_to_meas = #{}, type_date_to_meas = #{}, type_station_to_meas = #{}}).
+-record(stations, {all = sets:new(), coord_to_elem = #{}, name_to_elem = #{}}).
+-record(monitor, {meas = #measurements{}, stats = #stations{}}).
+
+
+
+reverse_nil_test() -> [] = lists:reverse([]).
+reverse_one_test() -> [1] = lists:reverse([1]).
+reverse_two_test() -> [2,1] = lists:reverse([1,2]).
+
+length_test() -> ?assert(length([1,2,3]) =:= 3).
+
+create_monitor_test() ->
+  Current = pollution:create_monitor(),
+  Expected = {
+    monitor,
+    {measurements,
+      sets:new(),
+      #{},
+      #{},
+      #{}
+    },
+    {stations,
+      sets:new(),
+      #{},
+      #{}
+    }
+  },
+  ?assertEqual(Current, Expected).
+
+add_station_test() ->
+  Monitor = pollution:create_monitor(),
+  Current = pollution:add_station("Lodolamacz Moskwa", {14, 10}, Monitor),
+  ?assert(sets:is_element({"Lodolamacz Moskwa", {14, 10}}, (Current#monitor.stats)#stations.all)),
+  ?assert(maps:get("Lodolamacz Moskwa",
+    (Current#monitor.stats)#stations.name_to_elem) =:= {"Lodolamacz Moskwa", {14, 10}}),
+  ?assert(maps:get({14, 10},
+    (Current#monitor.stats)#stations.coord_to_elem) =:= {"Lodolamacz Moskwa", {14, 10}}).
+
