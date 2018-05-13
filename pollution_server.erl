cf2cda5ffbb3ea7e31f1445362a02df0cdb4f71f
osdnk <micosa97@gmail.com>
2018-05-13 11:32:29 +0200

    Add workable server with tests

---
+++ pollution_server.erl
@@ -0,0 +1,122 @@
+%%%-------------------------------------------------------------------
+%%% @author osdnk
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. May 2018 10:23
+%%%-------------------------------------------------------------------
+-module(pollution_server).
+-export([
+  init/0,
+  stop/0,
+  crash/0,
+  add_station/2,
+  add_value/4,
+  remove_value/3,
+  get_one_value/3,
+  get_station_mean/2,
+  get_daily_mean/2,
+  get_air_quality_index/2,
+  start/0
+]).
+
+%% API start
+
+
+stop() ->
+  handler(stop, []).
+
+add_station(Name, Station) ->
+  handler(add_station, [Name, Station]).
+
+add_value(Station, Date, Type, Value) ->
+  handler(add_value, [Station, Date, Type, Value]).
+
+remove_value(Station, Date, Type) ->
+  handler(remove_value, [Station, Date, Type]).
+
+get_one_value(Type, Date, Station) ->
+  handler(get_one_value, [Type, Date, Station]).
+
+get_station_mean(Type, Station) ->
+  handler(get_station_mean, [Type, Station]).
+
+get_daily_mean(Type, Day) ->
+  handler(get_daily_mean, [Type, Day]).
+
+get_air_quality_index(Date, Station) ->
+  handler(get_air_quality_index, [Date, Station]).
+
+crash() ->
+  %% io:format("pollution_server:crash()~n"),
+  handler(crash, []).
+
+%% API end
+
+start() ->
+  register(server, spawn(fun () -> init() end)),
+  server.
+
+init() ->
+  Monitor = pollution:create_monitor(),
+  loop(Monitor).
+
+handler(RequestType, Args) when is_list(Args) ->
+  server ! {RequestType, self(), Args},
+  receive
+    Message -> Message
+  after
+    1000 -> {error, timeeout}
+  end.
+
+response(Pid, Monitor) ->
+  Pid ! {ok, Monitor}.
+
+loop(Monitor) ->
+  receive
+    {add_station, Pid, [Name, Station]} ->
+      NewMonitor = pollution:add_station(Name, Station, Monitor),
+      response(Pid, NewMonitor),
+      loop(NewMonitor);
+
+    {add_value, Pid, [Station, Date, Type, Value]} ->
+      NewMonitor = pollution:add_value(Station, Date, Type, Value, Monitor),
+      response(Pid, NewMonitor),
+      loop(NewMonitor);
+
+    {remove_value, Pid, [Station, Date, Type]} ->
+      NewMonitor = pollution:remove_value(Station, Date, Type, Monitor),
+      response(Pid, NewMonitor),
+      loop(NewMonitor);
+
+    {get_one_value, Pid, [Type, Date, Station]} ->
+      NewMonitor = pollution:get_one_value(Type, Date, Station, Monitor),
+      response(Pid, NewMonitor),
+      loop(NewMonitor);
+
+    {get_station_mean, Pid, [Type, Station]} ->
+      NewMonitor = pollution:get_station_mean(Type, Station, Monitor),
+      response(Pid, NewMonitor),
+      loop(NewMonitor);
+
+    {get_daily_mean, Pid, [Type, Day]} ->
+      NewMonitor = pollution:get_daily_mean(Type, Day, Monitor),
+      response(Pid, NewMonitor),
+      loop(NewMonitor);
+
+    {get_air_quality_index, Pid, [Date, Station]} ->
+      NewMonitor = pollution:get_air_quality_index(Date, Station, Monitor),
+      response(Pid, NewMonitor),
+      loop(NewMonitor);
+
+    {stop, Pid, []} ->
+      response(Pid, Monitor),
+      terminate()
+
+%    {crash, Pid, []} ->
+    %response(Pid, 1 / 0)
+  end.
+
+terminate() ->
+  ok.
---
+++ pollution_server_test.erl
@@ -0,0 +1,96 @@
+%%%-------------------------------------------------------------------
+%%% @author osdnk
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. May 2018 10:52
+%%%-------------------------------------------------------------------
+-module(pollution_server_test).
+-author("osdnk").
+-include_lib("eunit/include/eunit.hrl").
+
+
+-record(measurements, {all = sets:new(), type_date_station_to_meas = #{}, type_date_to_meas = #{}, type_station_to_meas = #{}}).
+-record(stations, {all = sets:new(), coord_to_elem = #{}, name_to_elem = #{}}).
+-record(monitor, {meas = #measurements{}, stats = #stations{}}).
+
+
+
+add_value_test() ->
+  D = calendar:local_time(),
+  pollution_server:start(),
+  pollution_server:add_station("Lodolamacz Moskwa", {14, 10}),
+  pollution_server:add_value({14, 10}, D, "X", 12),
+  {ok, Current} = pollution_server:stop(),
+  ?assert(sets:is_element({{"Lodolamacz Moskwa", {14, 10}}, D, "X", 12}, (Current#monitor.meas)#measurements.all)),
+  ?assert(element(2, dict:find({"X", element(1, D)},
+    (Current#monitor.meas)#measurements.type_date_to_meas)) =:= [{{"Lodolamacz Moskwa", {14, 10}}, D, "X", 12}]).
+
+add_station_test() ->
+  pollution_server:start(),
+  pollution_server:add_station("Lodolamacz Moskwa", {14, 10}),
+  {ok, Current} = pollution_server:stop(),
+  ?assert(sets:is_element({"Lodolamacz Moskwa", {14, 10}}, (Current#monitor.stats)#stations.all)),
+  ?assert(maps:get("Lodolamacz Moskwa",
+    (Current#monitor.stats)#stations.name_to_elem) =:= {"Lodolamacz Moskwa", {14, 10}}),
+  ?assert(maps:get({14, 10},
+    (Current#monitor.stats)#stations.coord_to_elem) =:= {"Lodolamacz Moskwa", {14, 10}}).
+
+add_bad_station_test() ->
+  pollution_server:start(),
+  {ok, Current} = pollution_server:stop(),
+  ?assertNot(sets:is_element({"Lodolamacz Moskwa", {14, 10}}, (Current#monitor.stats)#stations.all)).
+
+remove_value_test() ->
+  D = calendar:local_time(),
+  pollution_server:start(),
+  pollution_server:add_station("Lodolamacz Moskwa", {14, 10}),
+  pollution_server:add_value({14, 10}, D, "X", 12),
+  pollution_server:remove_value({14, 10}, D, "X"),
+  {ok, Current} = pollution_server:stop(),
+  ?assertNot(sets:is_element({{"Lodolamacz Moskwa", {14, 10}}, D, "X", 12}, (Current#monitor.meas)#measurements.all)),
+  ?assert(element(2, dict:find({"X", D}, (Current#monitor.meas)#measurements.type_date_to_meas)) == []),
+  ?assert(element(2, dict:find({"X", {"Lodolamacz Moskwa", {14, 10}}}, (Current#monitor.meas)#measurements.type_station_to_meas)) == []).
+
+get_one_value_test() ->
+  D = calendar:local_time(),
+  pollution_server:start(),
+   pollution_server:add_station("Lodolamacz Moskwa", {14, 10}),
+  pollution_server:add_value({14, 10}, D, "X", 12),
+  {ok, C} = pollution_server:get_one_value("X", D, {14, 10}),
+  pollution_server:stop(),
+  ?assert(C =:= 12).
+
+get_station_mean_test() ->
+  D = calendar:local_time(),
+  pollution_server:start(),
+  pollution_server:add_station("Lodolamacz Moskwa", {14, 10}),
+  pollution_server:add_value({14, 10}, D, "X", 12),
+  pollution_server:add_value({14, 10}, calendar:universal_time_to_local_time({{2014, 7, 10}, {20, 29, 12}}), "X", 10),
+  {ok, C} =pollution_server:get_station_mean("X", "Lodolamacz Moskwa"),
+  pollution_server:stop(),
+  ?assert(C == 11).
+
+
+get_daily_mean_test() ->
+  D = calendar:local_time(),
+  pollution_server:start(),
+  pollution_server:add_station("Lodolamacz Moskwa", {14, 10}),
+  pollution_server:add_station("Lodolamacz Kijow", {19, 97}),
+  pollution_server:add_value({14, 10}, D, "X", 12),
+  pollution_server:add_value({19, 97}, D, "X", 10),
+  {ok, C} = pollution_server:get_daily_mean("X", element(1, D)),
+  pollution_server:stop(),
+  ?assert(C == 11).
+
+get_air_quality_index_test() ->
+  D1 =  calendar:universal_time_to_local_time({{2014, 7, 10}, {20, 30, 12}}),
+  D2 =  calendar:universal_time_to_local_time({{2014, 7, 10}, {20, 29, 12}}),
+  pollution_server:start(),
+  pollution_server:add_station("Lodolamacz Moskwa", {14, 10}),
+  pollution_server:add_value({14, 10}, D1, "PM10", 12),
+  pollution_server:add_value("Lodolamacz Moskwa", D2, "PM10", 10),
+  {ok, Current} = pollution_server:get_air_quality_index(D1, {14, 10}),
+  pollution_server:stop(),
+  ?assertEqual (Current, [0.24, 0]).
